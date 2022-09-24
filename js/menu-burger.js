d68068e4d639f80a0690fe7a194dc2838d46eb62
Alexander Aplemakh <sandro@parallels.com>
2022-09-24 09:16:59 +0400

    Fixes after youtube overview

--- js/menu-burger.js
+++ js/menu-burger.js
@@ -1,7 +1,13 @@
-document.querySelector(".menu-burger__btn").addEventListener("click", function () {
+document.querySelector(".menu-burger__btn").addEventListener("click", () => {
   document.querySelector(".menu-burger").classList.add("menu-burger--active");
 });
 
-document.querySelector(".menu-burger__close-btn").addEventListener("click", function () {
+document.querySelector(".menu-burger__close-btn").addEventListener("click", () => {
   document.querySelector(".menu-burger").classList.remove("menu-burger--active");
+});
+
+document.querySelectorAll(".menu-burger__lnk").forEach((menuItem) => {
+  menuItem.addEventListener("click", () => {
+    document.querySelector(".menu-burger").classList.remove("menu-burger--active");
+  });
 });
--- js/search.js
+++ js/search.js
@@ -1,7 +1,13 @@
 document.querySelector(".header-menu__search").addEventListener("click", function () {
   document.querySelector(".search-form").classList.add("search-form--active");
+  document.querySelector(".header-logo__wrapper").classList.add("header-logo__wrapper--search-form-active");
+  document.querySelector(".menu-burger__btn").classList.add("menu-burger__btn--search-form-active");
+  document.querySelector(".header-search__wrapper").classList.add("header-search__wrapper--search-form-active");
 });
 
 document.querySelector(".search-form__close-btn").addEventListener("click", function () {
   document.querySelector(".search-form").classList.remove("search-form--active");
+  document.querySelector(".header-logo__wrapper").classList.remove("header-logo__wrapper--search-form-active")
+  document.querySelector(".menu-burger__btn").classList.remove("menu-burger__btn--search-form-active")
+  document.querySelector(".header-search__wrapper").classList.remove("header-search__wrapper--search-form-active");
 });
