611f09695dec2e847a408b9c3751cba931e19e74
Alexander Aplemakh <sandro@parallels.com>
2022-10-08 10:54:57 +0400

    Eliminated duplication of main header menu

--- js/menu-burger.js
+++ js/menu-burger.js
@@ -1,13 +1,13 @@
 document.querySelector(".burger-btn").addEventListener("click", () => {
-  document.querySelector(".menu-burger").classList.add("menu-burger--active");
+  document.querySelector(".header-menu1").classList.add("header-menu1--active");
 });
 
-document.querySelector(".menu-burger__close-btn").addEventListener("click", () => {
-  document.querySelector(".menu-burger").classList.remove("menu-burger--active");
+document.querySelector(".header-menu1__close-btn").addEventListener("click", () => {
+  document.querySelector(".header-menu1").classList.remove("header-menu1--active");
 });
 
-document.querySelectorAll(".menu-burger__lnk").forEach((menuItem) => {
+document.querySelectorAll(".header-menu1__lnk").forEach((menuItem) => {
   menuItem.addEventListener("click", () => {
-    document.querySelector(".menu-burger").classList.remove("menu-burger--active");
+    document.querySelector(".header-menu1").classList.remove("header-menu1--active");
   });
 });
