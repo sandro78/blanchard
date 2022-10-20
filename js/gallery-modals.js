86e261700acbae91d950c9bb01d8e91c8c0dedc3
Alexander Aplemakh <sandro@parallels.com>
2022-10-20 06:50:02 +0400

    Gallery modals enhances:
    1. Prevented scrolling of body when modal is open
    2. Added closing of modal not only on close button but also on overlay click

--- js/gallery-modals.js
+++ js/gallery-modals.js
@@ -2,6 +2,7 @@ const picturesBtns = document.querySelectorAll('.gallery__swiper-slide');
 const modalOverlay = document.querySelector('.gallery-modal-overlay ');
 const modals = document.querySelectorAll('.gallery-modal');
 const closeModalBtns = document.querySelectorAll('.gallery-modal__close-btn');
+const bodyElement = document.querySelector('body');
 
 picturesBtns.forEach((el) => {
   el.addEventListener('click', (e) => {
@@ -13,15 +14,20 @@ picturesBtns.forEach((el) => {
 
     document.querySelector(`[data-target="${path}"]`).classList.add('gallery-modal--visible');
     modalOverlay.classList.add('gallery-modal-overlay--visible');
+    bodyElement.style.overflow = 'hidden';
   });
 });
 
-closeModalBtns.forEach((el) => {
-  el.addEventListener('click', () => {
-
-    modalOverlay.classList.remove('gallery-modal-overlay--visible');
-    modals.forEach((el) => {
-      el.classList.remove('gallery-modal--visible');
-    });
+const closeModalFunc = function () {
+  modalOverlay.classList.remove('gallery-modal-overlay--visible');
+  modals.forEach((el) => {
+    el.classList.remove('gallery-modal--visible');
   });
+  bodyElement.style.overflow = "auto";
+}
+
+modals.forEach((el) => el.addEventListener('click', (e) => e.stopPropagation()));
+modalOverlay.addEventListener('click', closeModalFunc);
+closeModalBtns.forEach((el) => {
+  el.addEventListener('click', closeModalFunc);
 });
